2fea835ef8248a4f73dc7e1e9d3592bf12823a0e
Felix Oduor <felixochieng21@gmail.com>
2024-01-24 10:34:06 +0300

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -41,6 +42,7 @@ function App() {
       </Show>
 
       <GridItem area="main">
+        <GameHeading  gameQuery={gameQuery}/>
         <Flex marginBottom={5}>
           <Box marginRight={5}>
             <PlatformSelector
